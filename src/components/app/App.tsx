d723e0cc46939d96798cc2319a8ebd3ca55b3afd
DenysNazarenko <dinis.nazarenko03@gmail.com>
2024-12-26 10:58:24 +0200

    first commit

---
+++ src/components/app/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eos ipsam est natus cum, repellat voluptatibus tenetur nesciunt sequi sint quibusdam nobis ipsa. Dolores laudantium assumenda rerum quos, placeat eius?
+    </div>
+  );
+}
+
+export default App;
